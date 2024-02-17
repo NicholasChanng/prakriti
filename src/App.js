9416745769b5e2cee59847e4289cb166c49d4598
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2024-02-17 16:33:54 -0500

    1.1
    Banana

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "../src/components/Format/Layout/layout";
-import Header from "./components/Format/Header/header";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        {/* <Route path='/mission-statement' element={<Mission/>}> */}
+        </Route>
     </Routes>
   );
 }
---
+++ src/components/Format/Mission Statement/mission.js
@@ -0,0 +1,14 @@
+import "./header.css";
+
+export default function Mission() {
+  return (
+    <div class="mission-cont">
+        <h1 class="titleM">
+            Our Mission
+        </h1>
+        <p>
+
+        </p>
+    </div>
+  );
+}
