ce1e01f44c3a6f94b32298f92f5f18e36b62f336
Nicholas Channg <nicholas.channg@gmail.com>
2024-02-17 17:22:47 -0500

    1.6
    Fonts

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,8 +1,8 @@
 export default function Logo() {
   return (
     <svg
-      width="200"
-      height="200"
+      width="50"
+      height="50"
       viewBox="0 0 200 200"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
