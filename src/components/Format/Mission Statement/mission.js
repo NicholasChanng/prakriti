9a75b078fcd396c6ac3a0fcd4171534d1b61ce5a
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2024-02-17 17:50:31 -0500

    Easy
    Clap

--- src/components/Format/Mission Statement/mission.js
+++ src/components/Format/Mission Statement/mission.js
@@ -1,17 +1,21 @@
 import "./mission.css";
+import FadeInSection from "./experiment"
+import React from "react";
 
 export default function Mission() {
   return (
     <div className="container1">
-        <div className="mission-cont">
-            <h1 className="textM">
-                <strong>Our Mission</strong>
-            </h1>
-            <p className="textM">
-                Yap Yap Yap
-            </p>
-        </div>
-        <img className="mushImg" src="mush.gif" alt="mush" />
+        <FadeInSection>
+            <div className="mission-cont">
+                <h1 className="textM">
+                    <strong>Our Mission</strong>
+                </h1>
+                <p className="textM">
+                    Yap Yap Yap
+                </p>
+            </div>
+            <img className="mushImg" src="mush.gif" alt="mush" />
+        </FadeInSection>
     </div>
   );
 }
