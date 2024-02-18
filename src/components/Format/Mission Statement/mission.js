16f39ab00e3e52ecd5c6fa1424ccfbf5b3a2532a
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2024-02-17 19:47:39 -0500

    Deleted Stuff
    Strong replace p

--- src/components/Format/Mission Statement/mission.js
+++ src/components/Format/Mission Statement/mission.js
@@ -5,8 +5,8 @@ export default function Mission() {
   return (
     <div className="container1">
         <div className="mission-cont">
-                <h1 className="textM">
-                    <strong>Green Fields, Clear Skies: Empowering Growth & Feeding the Future.</strong>
+                <h1>
+                    <p className="textM">Green Fields, Clear Skies: Empowering Growth & Feeding the Future.</p>
                 </h1>
         </div>
                 <img className="mushImg" src="mush.gif" alt="mush" />
