32db4036412f0b06789a947315d1c1f748a29e80
Nicholas Channg <nicholas.channg@gmail.com>
2024-02-17 21:54:52 -0500

    1.10
    Added a lot

--- src/components/Format/Footer/footer.js
+++ src/components/Format/Footer/footer.js
@@ -5,31 +5,33 @@ import github from "../../../assets/img/github.png";
 
 export default function Footer() {
   return (
-    <div className="container footer">
-      <div className="footer-logo">
-        <Logo />
-        <span>PRAKRITI</span>
-      </div>
-      <div className="footer-links">
-        <div className="pages">
-          <span>Pages</span>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+    <section>
+      <div className="container footer">
+        <div className="footer-logo">
+          <Logo />
+          <span>PRAKRITI</span>
         </div>
-        <div className=""></div>
+        <div className="footer-links">
+          <div className="pages">
+            <span>Pages</span>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <div className=""></div>
+        </div>
+        <span className="copyright">
+          &copy;{new Date().getFullYear()} PRAKRITIT
+        </span>
+        <a
+          className="github"
+          href="https://github.com/NicholasChanng/prakriti"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img id="github" src={github} alt="github" />
+        </a>
       </div>
-      <span className="copyright">
-        &copy;{new Date().getFullYear()} PRAKRITIT
-      </span>
-      <a
-        className="github"
-        href="https://github.com/NicholasChanng/prakriti"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img id="github" src={github} alt="github" />
-      </a>
-    </div>
+    </section>
   );
 }
---
+++ src/components/Format/Home/home.js
@@ -0,0 +1,32 @@
+import Mission from "./Mission Statement/mission";
+import "./home.css";
+
+import tree from "../../../assets/img/tree.png";
+
+export default function Home() {
+  return (
+    <>
+      <section className="mission-statement">
+        <div className="container">
+          <Mission />
+        </div>
+      </section>
+      <section className="problem-section">
+        <div className="container">
+          <div className="problem-area">
+            <div className="problem-description">
+              <span>Empowering rural India to protect the air</span>
+              <br />
+              <br />
+              <br />
+              Prakrati supplies resources for Indian farmers to repurpose rice
+              crop stubble into reliable food sources, reducing smoke pollution
+              from burning crops.
+            </div>
+            <img id="tree" src={tree} alt="tree" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
