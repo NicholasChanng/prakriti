e2cee688b275cb67f9cee5653cc0252fe57c9892
Nicholas Channg <nicholas.channg@gmail.com>
2024-02-18 00:57:42 -0500

    1.12
    Popup added.

--- src/components/Format/Home/home.js
+++ src/components/Format/Home/home.js
@@ -1,9 +1,19 @@
 import Mission from "./Mission Statement/mission";
 import "./home.css";
+import { React, useState } from "react";
 
 import tree from "../../../assets/img/tree.png";
 
 export default function Home() {
+  const [popup, setPopup] = useState(false);
+
+  document.addEventListener("keydown", function (event) {
+    if (popup) {
+      if (event.keyCode === 27) {
+        setPopup(!popup);
+      }
+    }
+  });
   return (
     <>
       <section className="mission-statement">
@@ -21,11 +31,32 @@ export default function Home() {
               Prakrati supplies resources for Indian farmers to repurpose rice
               crop stubble into reliable food sources, reducing smoke pollution
               from burning crops.
+              <br />
+              <br />
+              <div
+                className="problem-btn"
+                onClick={() => {
+                  setPopup(!popup);
+                }}
+              >
+                More Info
+              </div>
             </div>
             <img id="tree" src={tree} alt="tree" />
           </div>
         </div>
       </section>
+      <div className={popup ? "popup show-popup" : "popup hidden-popup"}>
+        <div
+          className="exit"
+          onClick={() => {
+            setPopup(!popup);
+          }}
+        >
+          <div className="x top"></div>
+          <div className="x bottom"></div>
+        </div>
+      </div>
     </>
   );
 }
