4566bf574fcd11be82e7e62c6dfeea0ad3d321d8
Anthony Paredes-Bautista <anthonyparedesb0@gmail.com>
2024-02-17 19:38:31 -0500

    Diff Effects
    Got rid of fadein function

--- src/components/Format/Mission Statement/mission.js
+++ src/components/Format/Mission Statement/mission.js
@@ -1,20 +1,15 @@
 import "./mission.css";
-import FadeInSection from "../../experiment"
 import React from "react";
 
 export default function Mission() {
   return (
     <div className="container1">
         <div className="mission-cont">
-            <FadeInSection>
                 <h1 className="textM">
                     <strong>Green Fields, Clear Skies: Empowering Growth & Feeding the Future.</strong>
                 </h1>
-            </FadeInSection>
         </div>
-            <FadeInSection>
                 <img className="mushImg" src="mush.gif" alt="mush" />
-            </FadeInSection>
         </div>
   );
 }
