ddb0be8f592345c81a0e13f5fb4c725650cd0d23
Nicholas Channg <87846996+NicholasChanng@users.noreply.github.com>
2024-07-13 02:49:54 -0400

    Changed to bumi branding

--- src/components/Format/Home/home.js
+++ src/components/Format/Home/home.js
@@ -30,9 +30,9 @@ export default function Home() {
               <span>Empowering rural India to protect the air</span>
               <br />
               <br />
-              Prakrati supplies resources for Indian farmers to repurpose rice
-              crop stubble into reliable food sources, reducing smoke pollution
-              from burning crops.
+              Bumi supplies resources for Indian farmers to repurpose rice crop
+              stubble into reliable food sources, reducing smoke pollution from
+              burning crops.
               <br />
               <br />
               <div
