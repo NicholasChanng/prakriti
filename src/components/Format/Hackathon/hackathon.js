006190badaf5a53316a8c4426b357146d262af9c
Nicholas Channg <87846996+NicholasChanng@users.noreply.github.com>
2024-08-09 21:27:58 -0400

    Added blog

--- src/components/Format/Hackathon/hackathon.js
+++ src/components/Format/Hackathon/hackathon.js
@@ -3,7 +3,15 @@ import "./hackathon.css";
 export default function Hackathon() {
   return (
     <>
-      <div></div>
+      <div className="hackathon">
+        <a
+          href="https://news.cornell.edu/stories/2024/02/freshmen-win-top-prize-digital-ag-hackathon"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>Read About Our Hackathon Win!</p>
+        </a>
+      </div>
     </>
   );
 }
